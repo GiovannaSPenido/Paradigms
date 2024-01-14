120e68f7209f1a42dc04c006e13904f6c8c97f0c
Giovanna Penido <127809447+GiovannaSPenido@users.noreply.github.com>
2024-01-14 16:04:19 -0300

    Delete Programming with JAVA/TR_QuedaLivre directory

--- Programming with JAVA/TR_QuedaLivre/QuedaLivreController.java
+++
@@ -1,8 +0,0 @@
-package TR_QuedaLivre;
-
-public class QuedaLivreController {
-    static Passageiro[] passageiros = new Passageiro[10];
-    public static void cadastrarPassageiros() {
-        passageiros[1] = new Passageiro("647.762.371-48", "Cauã", "Avenida Perimetral - 409","20:00pm",  96516-6968, 100,"A1","V1");
-    }
-}
